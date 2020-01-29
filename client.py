991b1a2dccc66d01494edfb65288a515a1421634
vm-raccoon <vm.raccoon@gmail.com>
2020-01-29 10:36:50 +0200

    Add client.py

---
+++ client.py
@@ -0,0 +1,30 @@
+import socket
+import sys
+import threading
+import random
+from config import config
+
+
+def run_socket(index, config):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = (config['server'], config['port'])
+    print(f'{index} connecting to %s port %s' % server_address)
+    sock.connect(server_address)
+    try:
+        for i in range(0, 10): # * random.randint(1, 5)):
+            message = '1234567\n'.encode()
+            print(f'{index} sending "%s"' % message)
+            sock.sendall(message)
+            amount_received = 0
+            amount_expected = len(message)
+            while amount_received < amount_expected:
+                data = sock.recv(8)
+                amount_received += len(data)
+            print(f'{index} received "%s"' % data)
+    finally:
+        print(f'{index} closing socket')
+        sock.close()
+
+
+for k in range(0, 1):
+    threading.Thread(target=run_socket, args=(k, config,)).start()
