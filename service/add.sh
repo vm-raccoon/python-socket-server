3433c89eaa769fa27837dfb40b8a2d0df6f4a54b
vm-raccoon <vm.raccoon@gmail.com>
2020-01-29 10:35:31 +0200

    Add service files

---
+++ service/add.sh
@@ -0,0 +1,4 @@
+sudo cp ./pysocket.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable pysocket
+
---
+++ service/remove.sh
@@ -0,0 +1,4 @@
+sudo systemctl disable pysocket
+sudo rm /etc/systemd/system/pysocket.service
+sudo systemctl daemon-reload
+
